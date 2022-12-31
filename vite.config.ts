17a4c1492829e858e08b013b16f61f775922bb5b
Will Tatum <wtatum@prometheusgroup.com>
2022-12-31 14:04:29 -0500

    Move HTTPS config to local 'serve' command only

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig, UserConfigExport } from 'vite';
+import * as kit from '@sveltejs/kit';
+import { sveltekit } from '@sveltejs/kit/vite';
+import fs from 'fs';
+
+export default defineConfig(({ command, mode, ssrBuild }) => {
+
+	const config: UserConfigExport = {
+		plugins: [sveltekit()]
+	};
+	
+	// Set up local HTTPS so browser secure features are available for testing
+	if (command === 'serve') {
+		config.server = {
+			https: {
+				key: fs.readFileSync("../cert/asriel.coyote-little.ts.net.key"),
+				cert: fs.readFileSync("../cert/asriel.coyote-little.ts.net.crt")
+			}
+		};
+	}
+	
+	return config;
+});
