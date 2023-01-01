39636997e1aa97697ef82b1338ec769d4e65eb25
Will Tatum <wtatum@prometheusgroup.com>
2023-01-01 14:24:07 -0500

    Some QR Code logging

--- src/routes/sheet/raw/+server.ts
+++ src/routes/sheet/raw/+server.ts
@@ -3,6 +3,7 @@ import QRCode from "qrcode";
 import { v4 as uuidv4 } from "uuid";
 
 export const GET = async () => {
+    console.log("server generate code sheet PDF");
     return new Response(await doc(3) as any);
 }
 
@@ -20,6 +21,7 @@ const doc = async (pages: number) => {
 }
 
 const page = async (doc: typeof PDFDocument) => {
+    console.log("server add page");
     doc.addPage();
     
     doc.scale(5);
@@ -57,6 +59,8 @@ const drawQR = async (doc: typeof PDFDocument, value: string) => {
         type: "svg",
         margin: 0
     });
+
+    console.log("qr string", qr.length);
 
     // Assume the second match is the code (first is the border)
 
